9d88d9c20ebb2021f90600a145a4d48ddc30894d
nimeshmahato <98174768+nimeshmahato@users.noreply.github.com>
2025-10-27 14:14:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,14 @@
+let score ="33abc"
+let valueinNumber=Number(score)
+//console.log(typeof valueinNumber)
+//console.log(valueinNumber)
+
+//"33"-33
+//true-1,false-0
+let loggedin= 1
+let boolloggedin= Boolean(loggedin)
+
+console.log(boolloggedin)
+
+//"" -> false
+//"n" ->true
