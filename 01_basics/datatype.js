72b879f1d9816b2695ba8f8d621702109bf973bb
nimeshmahato <98174768+nimeshmahato@users.noreply.github.com>
2025-11-04 07:06:05 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,14 @@
+// Primitivr
+
+// 7types:String, Number, Boolean, null, undefined,symbol, BigInt
+const id=Symbol('123')
+const anoId=Symbol('123')
+
+//console.log(id===anoId);
+//reference(Non primitive)
+
+
+
+const myFunction=function(){
+    console.log("hello world")
+}
