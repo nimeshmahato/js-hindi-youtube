7323934739a68955e26b2e5bf3414d2bd523e4f8
nimeshmahato <98174768+nimeshmahato@users.noreply.github.com>
2025-11-05 14:58:51 +0000

    math and num

---
+++ 01_basics/06_number.js
@@ -0,0 +1,21 @@
+const score=400
+// console.log(score);
+// const balance = new Number(100.65889)
+// console.log(balance);
+// console.log(balance.toFixed(2));
+
+const othernumber=123.656
+// console.log(othernumber.toPrecision(4))
+
+
+//////////////////////////////////////////////
+
+console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.random())
+
+const min=10
+const max=21
+
+console.log(Math.floor(Math.random() * (max-min+1))+ min)
