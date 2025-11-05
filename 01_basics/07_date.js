0252a14636457d8ef9a04c71be4dba1dae69fb37
nimeshmahato <98174768+nimeshmahato@users.noreply.github.com>
2025-11-05 15:35:14 +0000

    date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,22 @@
+// Dates
+
+let mydate=new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+
+// console.log(typeof mydate)
+
+let createddate=new Date(2025,0,15)
+// console.log(createddate.toDateString());
+
+// let myTimeStamp= Date.now()
+// // console.log(myTimeStamp)
+// // console.log(createddate.getTime());
+// console.log(Math.floor(Date.now()/1000))
+
+let newdate= new Date()
+console.log(newdate)
+console.log(newdate.getMonth()+1)
+console.log(newdate.getDay())
+console.log(newdate.toLocaleString())
